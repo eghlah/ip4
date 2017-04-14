cb2af6843c91a3e776cf958ccddcfad978e0c9c6
eghlah <doreenthiuru@gmail.com>
2017-04-14 18:01:09 +0300

    js to calculate input file

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var input_no = document.getElementById("inputone").value;
+
+  for (int i = 0; i < input_no; i++) {
+
+    if (i % 3 == 0) {
+      document.getElementById("result").innerHTML = "ping";
+    }
+    text += cars[i] + "<br>";
+
+
+  }
+
+
+}
